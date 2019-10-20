c493f7940c9ea4c10b1cb25ca74fd65e91f80250
theone3nu <mytu3nu@gmail.com>
2019-10-20 14:41:12 +0530

    getUserinfo

--- engine/routes.js
+++ engine/routes.js
@@ -3,7 +3,7 @@
 //Define all the routes in the server running on multichain cluster
 module.exports = function routes(app) {
   //GET routes
-  app.get('/getUserInfo', require('./data/query').fetchDataByKey);
+  app.get('/getUserInfo', require('./users/userInfo').getUserInfo);
   app.get(
     '/',
     require('./common/middleware').checkToken,
