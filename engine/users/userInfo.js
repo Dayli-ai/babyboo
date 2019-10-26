c97b537887fec01ff33b26f0d750eb2a727cb399
theone3nu <mytu3nu@gmail.com>
2019-10-26 21:17:01 +0530

    issue vaccine api implementation.

--- engine/users/userInfo.js
+++ engine/users/userInfo.js
@@ -1,16 +1,24 @@
 const axios = require('axios');
 const url = 'http://34.207.213.121:3000'
+const checkToken = require('../common/middleware').checkToken;
 
 exports.getUserInfo = async (req, res) => {
+  const { authorization } = req.headers;
+  checkToken(req, res, async result => {
+    if(result.success) {
+      const {stream, key} = req.query;
+      const response = await axios.get(`${url}/queryDataByKey?stream=${stream}&key=${key}`);
+      const items = response.data.items;
+      let data = {}
+      if (items.length > 0) {
+        data = JSON.parse(items[items.length -1].data);
+      }
+      res.json(data);
+    }else {
+      res.json(result);
+    }
+  });
 
-  const {stream, key} = req.query;
-  const response = await axios.get(`${url}/queryDataByKey?stream=${stream}&key=${key}`);
-  const items = response.data.items;
-  let data = {}
-  if (items.length > 0) {
-     data = JSON.parse(items[items.length -1].data);
-  }
-  res.json(data);
 }
 
 
