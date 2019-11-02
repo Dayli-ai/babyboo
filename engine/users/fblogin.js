89734b7f2d6146b5d2b9a9060e8cb524e17aec97
theone3nu <mytu3nu@gmail.com>
2019-11-02 16:59:12 +0530

    auto register when user tries to login with fb for the first time

--- engine/users/fblogin.js
+++ engine/users/fblogin.js
@@ -1,16 +1,30 @@
 const axios = require('axios');
-
-const url = 'http://34.207.213.121:3000'
-
+const url = 'http://34.207.213.121:3000';
 exports.fblogin = async function(req, res) {
   let username = req.body.username;
 
   const data = { username };
   let response;
   try {
-    response = await axios.post(`${url}/fblogin`, data);
-    console.log(response);
-    res.json(response.data);
+    const stream = 'bb_stream';
+    const qureyResponse = await axios.get(`${url}/queryDataByKey?stream=${stream}&key=${username}`);
+    const items = qureyResponse.data.items;
+    if (items.length > 0) { //if user found then direct login into app
+      response = await axios.post(`${url}/fblogin`, data);
+      console.log(response);
+      res.json(response.data);
+    }else { //if user not found then register user into babyapp with password dummy and login into app.
+      data.password = 'dummy';
+      const userData = {
+        "stream": "bb_stream",
+        "key": username,
+        "data":data
+      };
+      await axios.post(`${url}/registerUser`, userData);
+      const loginResponse = await axios.post(`${url}/fblogin`, data);
+      res.json(loginResponse.data);
+    }
+
   }catch(e) {
     res.status(403).json(e.response.data);
   }
