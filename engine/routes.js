f13fad6fad138fd6163b6616008f40b91bcdbabb
theone3nu <mytu3nu@gmail.com>
2020-01-06 05:16:23 +0530

    push notifications implementation

--- engine/routes.js
+++ engine/routes.js
@@ -1,21 +1,33 @@
 'use strict';
-var multer  = require('multer');
+const pushnotifications = require('./users/pushnotifications');
 
+function setTimerForNotifications() {
+  setInterval(() => {
+    console.log('Triggering Notifications');
+    pushnotifications.checkAndPushNotifications();
+  }, 86400 * 1000);
+}
+pushnotifications.checkAndPushNotifications(); //Initial call
+setTimerForNotifications();
+const multer = require('multer');
 //Define all the routes in the server running on multichain cluster
 const Storage = multer.diskStorage({
   destination(req, file, callback) {
-    callback(null, './images')
+    callback(null, './images');
   },
   filename(req, file, callback) {
-    callback(null, `${file.fieldname}_${Date.now()}_${file.originalname}`)
+    callback(null, `${file.fieldname}_${Date.now()}_${file.originalname}`);
   },
-})
+});
 
-const upload = multer({ storage: Storage })
+const upload = multer({ storage: Storage });
 module.exports = function routes(app) {
   //GET routes
   app.get('/getUserInfo', require('./users/userInfo').getUserInfo);
-  app.get('/getMilestoneData', require('./users/milestoneInfo').getMilestoneInfo);
+  app.get(
+    '/getMilestoneData',
+    require('./users/milestoneInfo').getMilestoneInfo,
+  );
   app.get(
     '/',
     require('./common/middleware').checkToken,
@@ -24,14 +36,24 @@ module.exports = function routes(app) {
 
   //POST routes
   app.post('/registerUser', require('./users/register').createUser);
-  app.post('/updateMilestone', require('./users/updateMilestone').updateMilestone);
+  app.post(
+    '/updateMilestone',
+    require('./users/updateMilestone').updateMilestone,
+  );
   app.post('/login', require('./users/login').login);
   app.post('/fblogin', require('./users/fblogin').fblogin);
   app.post('/forgotPassword', require('./users/forgotPassword').forgotPassword);
   app.post('/resetPassword', require('./users/resetPassword').resetPassword);
 
   app.post('/registerChild', require('./users/registerChild').createChild);
-  app.post('/updateDeviceToken', require('./users/updateDeviceToken').updateDeviceToken);
+  app.post(
+    '/updateDeviceToken',
+    require('./users/updateDeviceToken').updateDeviceToken,
+  );
   app.post('/issueVaccine', require('./users/issueVaccine').issueVaccine);
-  app.post('/uploadImage', upload.array('photo', 3), require('./users/uploadImage').uploadImage);
+  app.post(
+    '/uploadImage',
+    upload.array('photo', 3),
+    require('./users/uploadImage').uploadImage,
+  );
 };
---
+++ engine/users/pushnotifications.js
@@ -0,0 +1,83 @@
+const axios = require('axios');
+const url = 'http://34.207.213.121:3002/queryStreamKeys?stream=bb_stream';
+const url1 = 'http://34.207.213.121:3000';
+
+async function asyncForEach(array, callback) {
+  for (let index = 0; index < array.length; index++) {
+    await callback(array[index], index, array);
+  }
+}
+
+const checkAndPushNotifications = async () => {
+  const result = await axios.get(url);
+  const users = result.data.items;
+  const stream = 'bb_stream';
+  console.log('Total Users', users.length);
+  await asyncForEach(users, async (user, index) => {
+    console.log('index', index);
+    try {
+      const response = await axios.get(
+        `${url1}/queryDataByKey?stream=${stream}&key=${user}`,
+      );
+      const items = response.data.items;
+      let data = {};
+      if (items.length > 0) {
+        data = JSON.parse(items[items.length - 1].data);
+      }
+
+      const { deviceToken, childVaccines } = data;
+      if (deviceToken && childVaccines) {
+        console.log(`UserInfo user ${user} deviceToken ${deviceToken}`);
+        let keys = [];
+        for (let key in childVaccines) {
+          keys.push(key);
+        }
+        await asyncForEach(keys, async (key, index) => {
+          const dueDate = childVaccines[key].dueDate;
+          const dueDateArray = dueDate.split("/");
+          const [dd, mm, yyyy ] = dueDateArray;
+          const dateFormatted = `${yyyy}-${mm}-${dd}`;
+          const childDueDate = new Date(dateFormatted);
+          const dueDateString = childDueDate.getDate() + '/' + (childDueDate.getMonth()+1)+ '/' + childDueDate.getFullYear();
+
+          const reminderDate = new Date().addDays(2); // To send reminder before 2 days
+          const reminderDateString = reminderDate.getDate() + '/' + (reminderDate.getMonth()+1) + '/' + reminderDate.getFullYear();
+          if(reminderDateString === dueDateString) {
+            // If reminder date is equal to due date send notification
+            console.log(`sending push notification for user ${user} ${key}`)
+            const notificationData = {
+              to: data.deviceToken,
+              notification: {
+                body: '48 HRS left for next vaccine, hurry up',
+                title: 'Baby Boo Vaccine Reminder',
+                content_available: true,
+                priority: 'high',
+              },
+              data: {
+                body: '48 HRS left for next vaccine, hurry up',
+                title: 'Baby Boo Vaccine Reminder',
+                content_available: true,
+                priority: 'high',
+              },
+            };
+            axios.defaults.headers.common[
+              'Authorization'
+              ] = `key=AAAAVD9hswU:APA91bGTDcpQtJiISU9yPvzdbb0Hkg9BjXGKtZ2rJQKxFUCTKXQqVPYhnx6nGs4kGxJlZBZaX2PPKSGKyqis9TSW_fjtcmft_BYweHts9shrjU_jdaTqPFHoSNXMxhxA3m-WxKWTbnkb`;
+            await axios.post(
+              'https://fcm.googleapis.com/fcm/send',
+              notificationData,
+            );
+          }
+        });
+
+      }
+      if (index === users.length - 1) {
+        console.log('End of Push notifications');
+      }
+    } catch (e) {
+      console.log('error for user ', user);
+    }
+  });
+};
+
+module.exports = { checkAndPushNotifications };
