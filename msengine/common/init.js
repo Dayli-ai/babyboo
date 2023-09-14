a5fcd6aefee78a9ca5804a4474790f41069a50f1
Ravilochan <ravilochan2000@gmail.com>
2023-09-14 20:27:04 +0530

    updated MC env variables

--- msengine/common/init.js
+++ msengine/common/init.js
@@ -3,38 +3,41 @@ var log4js = require('log4js');
 
 exports.getMultichain = function () {
     let multichain = require("multichain-node")({
-        port: process.env.PORT,
-        host: process.env.HOST,
-        user: process.env.USERNAME,
-        pass: process.env.PASSWORD,
+        port: process.env.MC_PORT,
+        host: process.env.MC_HOST,
+        user: process.env.MC_USERNAME,
+        pass: process.env.MC_PASSWORD,
         version: process.env.MC_VERSION
     });
     return multichain;
 };
 
 exports.getLog = function () {
-   let log = log4js.configure({ // configure to use all types in different files.
-      appenders: {
-        "backend-logs" : { type: "file",
-            filename: "logs/backend.log", // specify the path where u want logs folder error.log
-            category: 'info',
-            maxLogSize: 20480,
-            backups: 10
+    let log = log4js.configure({ // configure to use all types in different files.
+        appenders: {
+            "backend-logs": {
+                type: "file",
+                filename: "logs/backend.log", // specify the path where u want logs folder error.log
+                category: 'info',
+                maxLogSize: 20480,
+                backups: 10
+            },
+            "frontend-logs": {
+                type: "file",
+                filename: "logs/frontend.log", // specify the path where u want logs folder info.log
+                category: 'info',
+                maxLogSize: 20480,
+                backups: 10
+            }
         },
-        "frontend-logs": {   type: "file",
-            filename: "logs/frontend.log", // specify the path where u want logs folder info.log
-            category: 'info',
-            maxLogSize: 20480,
-            backups: 10
-        }},
         "categories": {
-		"backend-logs" :{"appenders": ["backend-logs"] ,"level": "info"},
-		"frontend-logs" :{"appenders": ["frontend-logs"] ,"level": "info"},
-		default :{"appenders": ["backend-logs"] ,"level": "info"}
-	//{[ "frontend-logs" ],"level": "info" }
-        //"appenders": {["frontend-logs"], "level": "info" }
+            "backend-logs": { "appenders": ["backend-logs"], "level": "info" },
+            "frontend-logs": { "appenders": ["frontend-logs"], "level": "info" },
+            default: { "appenders": ["backend-logs"], "level": "info" }
+            //{[ "frontend-logs" ],"level": "info" }
+            //"appenders": {["frontend-logs"], "level": "info" }
         }
-   }); 
-	return log;
+    });
+    return log;
 };
 
